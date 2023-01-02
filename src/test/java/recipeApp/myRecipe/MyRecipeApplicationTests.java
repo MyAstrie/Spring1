a1f5adfda2af5546eee5ecb5d2e45fce5406e5c1
Andrey Zinchenko <astrokoit@gmail.com>
2023-01-03 03:09:31 +0700

    Init commit

---
+++ src/test/java/recipeApp/myRecipe/MyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package recipeApp.myRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
